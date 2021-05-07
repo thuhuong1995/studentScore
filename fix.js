98a5704ba501ebb2c5e2c90dbd0057cacef928bf
GhostFury <thuhuongvnhubt@gmail.com>
2021-05-07 22:30:53 +0700

    fix

--- fix.js
+++ fix.js
@@ -3,51 +3,64 @@ function getRandomMark(min = 6, max = 10) {
 }
 numberRecordInput = 5;
 
-let studentList = Array.from(Array(numberRecordInput), (item, index) => ({
-  name: String.fromCharCode(65 + index),
-  scores: {
-    math: getRandomMark(),
-    literature: getRandomMark(),
-    physics: getRandomMark(),
-    chemistry: getRandomMark(),
-    biology: getRandomMark(),
-  },
-}));
+let studentList = Array.from(Array(numberRecordInput), (item, index) => {
+  let math = getRandomMark();
+  let literature = getRandomMark();
+  let physics = getRandomMark();
+  let chemistry = getRandomMark();
+  let biology = getRandomMark();
+  const averageScore =
+    (math * 2 + literature * 2 + physics + chemistry + biology) / 7;
 
-studentList.forEach((value, index) => {
-  studentList[index]["averageScore"] = averageScore(index);
-  studentList[index]["bonus"] = bonus(index);
+  return {
+    name: String.fromCharCode(65 + index),
+    scores: {
+      math,
+      literature,
+      physics,
+      chemistry,
+      biology,
+    },
+    averageScore,
+  };
 });
+// studentList.forEach((value, index) => {
+//   studentList[index]["bonus"] = bonus(index);
+// });
+
+// studentList.forEach((value, index) => {
+//   studentList[index]["totalScore"] = averageScore(index);
+// });
 
 window.addEventListener = ("load", renderList(studentList));
 
 function renderList(listStudent) {
   let studentList = `
-    <tr>
-        <td>Tên</td>
-        <td>Math Score</td>
-        <td>Literature Score</td>
-        <td>Physics Score</td>
-        <td>Chemistry Score</td>
-        <td>Byology Score</td>
-        <td>Total Score</td>
-        <td>Award</td>
-    </tr>
-    `;
+      <tr>
+          <td>Tên</td>
+          <td>Math Score</td>
+          <td>Literature Score</td>
+          <td>Physics Score</td>
+          <td>Chemistry Score</td>
+          <td>Byology Score</td>
+          <td>Total Score</td>
+          <td>Award</td>
+      </tr>
+      `;
   listStudent.forEach(
     (student, index) =>
       (studentList += `
-      <tr>
-          <td>${student.name}</td>
-          <td> ${student.scores.math}</td>
-          <td> ${student.scores.literature}</td>
-          <td> ${student.scores.physics}</td>
-          <td> ${student.scores.chemistry}</td>
-          <td> ${student.scores.biology}</td>
-          <td><p>${student.averageScore}</p></td>
-          <td><p>${student.bonus}</p></td>
-      </tr>
-      `)
+        <tr>
+            <td>${student.name}</td>
+            <td> ${student.scores.math}</td>
+            <td> ${student.scores.literature}</td>
+            <td> ${student.scores.physics}</td>
+            <td> ${student.scores.chemistry}</td>
+            <td> ${student.scores.biology}</td>
+            <td><p>${student.averageScore}</p></td>
+            <td><p>${student.bonus}</p></td>
+        </tr>
+        `)
   );
   document.getElementById("list-student").innerHTML = studentList;
 }
@@ -55,6 +68,7 @@ function renderList(listStudent) {
 function averageScore(index) {
   let currentStudent = studentList[index];
   let countCorse = studentList.length;
+
   const currentStudentScore = currentStudent.scores;
 
   if (currentStudent) {
@@ -94,12 +108,16 @@ function filterByCondition() {
 
   switch (sortByCondition) {
     case "searchGreatStudent":
-      let searchGreatStudent = studentList.filter(
-        (item) => item.averageScore >= 8
-      );
+      console.log(studentList);
+      let searchGreatStudent = studentList.filter((item) => {
+        console.log(item.averageScore, item.averageScore >= 8);
+
+        return item.averageScore >= 8;
+      });
       renderList(searchGreatStudent);
       break;
     case "selectBestStudent":
+      console.log(studentList);
       let selectBestStudent = studentList.sort((item1, item2) => {
         if (item1.averageScore > item2.averageScore) {
           return -1;
@@ -109,6 +127,7 @@ function filterByCondition() {
       renderList([bestStudent]);
       break;
     case "selectMinLiterature":
+      console.log(studentList);
       let selectMinLiterature = studentList.sort((item1, item2) => {
         if (item1.scores.literature < item2.scores.literature) {
           return -1;
@@ -125,6 +144,6 @@ function filterByCondition() {
 
 const totalBonus = studentList.reduce(sumBonus, 0);
 function sumBonus(bonusList, currentScore) {
-  return bonusList + currentScore.bonus;
+  return bonusList + Number(currentScore.bonus);
 }
 document.getElementById("totalBonus").innerHTML = "Total bonus : " + totalBonus;
